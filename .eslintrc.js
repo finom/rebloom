c82bff612fb6f3b704b5b766057c79dcbb2a125c
Andrii Gubanov <andrey.a.gubanov@gmail.com>
2023-12-21 22:23:01 +0100

    chore: Renew deps

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,22 +15,9 @@ module.exports = {
   parserOptions: {
     ecmaVersion: 2020,
     sourceType: 'module',
-    project: './tsconfig.json',
+    project: './tsconfig.eslint.json',
     // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
     tsconfigRootDir: __dirname,
     createDefaultProgram: true,
   },
-  /*
-  settings: {
-    'import/resolver': {
-      // See https://github.com/benmosher/eslint-plugin-import/issues/1396#issuecomment-575727774 for line below
-      node: {},
-      webpack: {
-        config: require.resolve('./.erb/configs/webpack.config.eslint.js'),
-      },
-    },
-    'import/parsers': {
-      '@typescript-eslint/parser': ['.ts', '.tsx'],
-    },
-  }, */
 };
